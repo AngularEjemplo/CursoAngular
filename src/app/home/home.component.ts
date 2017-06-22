b071dfcd9f945643a5ab204b42e228388183005a
Luis <luis.jojoa.q@gmail.com>
2017-06-22 17:54:05 -0500

    Primeros pasos para usar los servicios con Angular4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { RopaService } from '../servicios/ropa.service';
 @Component({
+
   selector: 'home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+
+  providers: [RopaService]
+
 })
 
 export class HomeComponent{
   public titulo = "Página principal";
+  public listado_ropa:Array<string>;
+  public prenda_guardar:string;
+
+  constructor(
+    private _ropaService: RopaService
+  ){}
+
+  ngOnInit(){
+    this.listado_ropa = this._ropaService.getRopa();
+    console.log(this.listado_ropa);
+  }
+
+  guardarPrenda(){
+    this._ropaService.addRopa(this.prenda_guardar);
+    this.prenda_guardar = null;
+  }
+
+  eliminarPrenda(index:number){
+    this._ropaService.deleteRopa(index);
+  }
 }
